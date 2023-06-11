8d7b28c7f0d819b1d3d8808ce89d8405dc26a2a8
tudeindra <gedeindramaheswara@gmail.com>
2023-06-12 02:57:44 +0800

    warga

--- koneksi.php
+++ koneksi.php
@@ -29,3 +29,8 @@ function registrasi($data){
         return false;
     }
 }
+function hapus($id){
+    global $conn;
+    mysqli_query($conn, "DELETE FROM warga WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
